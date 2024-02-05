c17980d3405e4fe27b46e5013e8b2f9b1829f263
Erick Tafel <97417443+ericktafel1@users.noreply.github.com>
2024-02-05 15:57:51 -0800

    Add files via upload

---
+++ Erick Tafel Personal Website/et.js
@@ -0,0 +1,6 @@
+var loader = document.getElementById('loader');
+window.addEventListener("load", function(event) {
+    loader.classList.remove('loading');
+    loader.classList.add('loaded');
+    document.body.classList.add('imgloaded');
+});
